fd9a0ee7de30e5ebc2f91cd14bda31bd2edffa69
Salah Eddine Bellafrouh <104754579+salaheddinebellafrouh@users.noreply.github.com>
2023-09-30 23:42:20 +0100

    some changes

--- draw/map_draw.c
+++ draw/map_draw.c
@@ -6,53 +6,48 @@
 /*   By: sbellafr <sbellafr@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 19:16:33 by ohachami          #+#    #+#             */
-/*   Updated: 2023/09/21 23:26:38 by sbellafr         ###   ########.fr       */
+/*   Updated: 2023/09/30 23:42:07 by sbellafr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
 
-int	angle_adjast(int angle, char sign)
+double	angle_adjast(double angle, char sign)
 {
-	if(angle <= 0)
-		angle = 360;
-	if(sign == '-')
-		angle--;
-	if(sign == '+')
-		angle++;
-	if(angle > 360)
-		angle = 1;
-	return(angle);
+	if (angle <= 0)
+		angle = M_PI * 2;
+	if (sign == '-')
+		angle -= M_PI / 180;
+	if (sign == '+')
+		angle += M_PI / 180;
+	if (angle > M_PI * 2)
+		angle = M_PI / 180;
+	return (angle);
 }
 
 void	draw_line(t_window *win, t_point start, t_point end)
 {
-	t_point	line;
-	float	big;
-	t_point	steps;
-	t_point	pixels;
+	t_point	dp;
+	t_point	pk;
+	double	step;
 	int		i;
 
-	i = 0;
-	line.x = end.x - start.x;
-	line.y = end.y - start.y;
-	if (abs(line.x) > abs(line.y))
-		big = abs(line.x);
+
+	dp.x = end.x - start.x;
+	dp.y = end.y - start.y;
+	pk.x = start.x;
+	pk.y = start.y;
+	if (fabs(dp.x) > fabs(dp.y))
+		step = fabs(dp.x) ;
 	else
-		big = abs(line.y);
-	steps.x = line.x / big;
-	steps.y = line.y / big;
-	pixels = start;
-	while (i < big)
+		step = fabs(dp.y);
+	i = -1;
+	while (++i < step)
 	{
-		printf("y=%d x=%d\n", (int)pixels.y / 50, (int)pixels.x / 50);
-		printf("y=%d x=%d\n", (int)start.y, (int)start.x);
-		if (win->map.mapo[(int)pixels.x / 50][(int)pixels.y / 50] == '1') // the problem is here pixels.y overflow
-			break ;
-		mlx_put_pixel(win->img, (int)pixels.x, (int)pixels.y, 0x1100FFff);
-		pixels.x += steps.x;
-		pixels.y += steps.y;
-		i++;
+	//ft_printf("x=%d,.,y=%d\n",(int)pk.x, (int)pk.y);
+		mlx_put_pixel(win->img, pk.x, pk.y, 0xFF322BFF);
+		pk.x += dp.x / step;
+		pk.y += dp.y / step;
 	}
 }
 
@@ -74,63 +69,141 @@ void	cub_drawer(mlx_image_t *img, t_point start, t_point end, int color)
 		i++;
 	}
 }
-
-t_point	player_drawer(mlx_image_t *img, t_point pos, t_player player,  int color)
+/*
+t_point	player_drawer(mlx_image_t *img, t_point pos, t_player player, int color)
 {
-	int	r;
-	float deg;
+	int		r;
+	float	deg;
 
 	r = -1;
 	deg = 0;
-	if(player.p.x != 0 || player.p.y != 0)
+	if (player.p.x != 0 || player.p.y != 0)
 		pos = player.p;
-	while(++r < 25)
-		mlx_put_pixel(img, pos.x + 25 ,pos.y + 25 + r, color);
+	while (++r < 25)
+		mlx_put_pixel(img, pos.x + 25 + r, pos.y + 25 + r, color);
 	r = 0;
-	while(r < 11)
+	while (r < 11)
 	{
 		deg = 0;
 		while (deg < M_PI * 2)
 		{
 			mlx_put_pixel(img, pos.x + 25 + r * cos(deg),
 				pos.y + 25 + r * sin(deg), color);
-			deg +=  M_PI / 180;
+			deg += M_PI / 180;
 		}
 		r++;
 	}
+	return (pos);
+}*/
+
+t_point	player_drawer(t_window *win, t_point pos, double angle, int color)
+{
+	int		r;
+	double	deg;
+
+	r = -1;
+	pos.x = (win->map.lenght * 50 + 10) * (pos.x < 1)
+		+ pos.x * (pos.x <= win->map.lenght * 50 + 20);
+	pos.y = (pos.y < 1) * (win->map.wide * 50 + 10)
+		+ pos.y * (pos.y <= win->map.wide * 50 + 20);
+	r = 0;
+	deg = 0;
+	while (r < 11)
+	{
+		deg = 0;
+		while (deg < M_PI * 2)
+		{
+			mlx_put_pixel(win->img, pos.x + r * cos(deg),
+				pos.y + r * sin(deg), color);
+			deg += M_PI / 180;
+		}
+		r++;
+	}
+	deg = angle;
 	return(pos);
 }
 
-/*
-t_point	player_drawer(mlx_image_t *img, t_point pos, t_player player,  int color)
+t_segm	wall(double sx, double sy, double ex, double ey)
 {
-	int	r;
-	float deg;
+	t_segm wall;
 
-	r = -1;
-	deg = angle_adjast(player.angle, '+');
-	if(player.p.x != 0 || player.p.y != 0)
-		pos = player.p;
-	while(++r < 25)
-		mlx_put_pixel(img, pos.x + 25 ,pos.y + 25 + r, color);
-	r = 0;
-	while(r < 11)
+	wall.end.x = ex;
+	wall.end.y = ey;
+	wall.start.x = sx;
+	wall.start.y = sy;
+	return (wall);
+}
+
+t_vector	rotation_vect(t_vector vect, double deg)
+{
+	t_vector new_vect;
+
+	new_vect.y = vect.y * cos(deg) - vect.x * sin(deg);
+	new_vect.x = vect.y * sin(deg) + vect.x * cos(deg);
+	/*if(new_vect.y <= -0.000000)
+		new_vect.y = 0;
+	if(new_vect.x <= -0.000000)
+		new_vect.x = 0;*/
+	return (new_vect);
+}
+int	dot_vect(t_vector vect, t_vector vect2)
+{
+	int dot;
+
+	dot = vect.y * vect2.y + vect.x * vect2.y;
+	return (dot);
+}
+
+void printooo(double lol)
+{
+	printf("|%.1f|\n",lol);
+}
+
+int	raycast(t_window *win, t_segm *edge, t_point *pos, double deg)
+{
+	t_vector v;
+	//t_vector n;
+	t_point r;
+	double t;
+	double d;
+	int i;
+
+	i = 7;
+	d = 0;
+	t = 0;
+	v.x = 1;
+	v.y = 0;
+	v = rotation_vect(v, deg);
+	//printf("x=%0.2f|y=%0.2f\n",pos->x,pos->y);
+	while (i > -1)
 	{
-		deg = angle_adjast(player.angle, '+');
-		mlx_put_pixel(img, pos.x + 25 + r * cos(deg),
-				pos.y + 25 + (r) * sin(deg++), color);
-		while (deg < M_PI * 2)
+		if (!v.x && !v.y)
+			break ;
+		d = (edge[i].end.y - edge[i].start.y) * v.x - (edge[i].end.x - edge[i].start.x) * v.y;
+		if (d < 0.00000000000001 && d > -0.0000000000001)
 		{
-			mlx_put_pixel(img, pos.x + 25 + r * cos(deg),
-				pos.y + 25 + r * sin(deg), color);
-			deg +=  M_PI / 180;
+			i--;
+			continue ;
 		}
-		r++;
+		d = ((pos->y - edge[i].start.y) * v.x - (pos->x - edge[i].start.x) * v.y) / d;
+		if (v.x > 0.00000000000001 || v.x < -0.0000000000001){
+			t = ((1 - d) * edge[i].start.x + d * edge[i].end.x - pos->x) / v.x;}
+		else if (v.y > 0.0000000000001 || v.y < -0.0000000000001){
+			t = ((1 - d) * edge[i].start.y + d * edge[i].end.y - pos->y) / v.y;}
+		if (d >= 0 && d <= 1 && t >= 0)
+			break ;
+		i--;
+		//n = rotation_vect(v, 90);
+		//if(dot_vect(v, n))
+		//	;
 	}
-	return(pos);
-}*/
+	r = assign_point(pos->x + t * v.x, pos->y + t * v.y);
+	draw_line(win, *pos, r);
+	//printf("t=%.2f|r.x=%.2f|r.y=%.2f|\n",t,r.x,r.y);
+	return (0);
+}
 
-void	ft_draw_map(t_window *win, char **matrix, t_player *player)
+void	ft_draw_map(t_window *win, char **matrix, t_point *pos)
 {
 	int		i;
 	int		j;
@@ -139,23 +212,39 @@ void	ft_draw_map(t_window *win, char **matrix, t_player *player)
 
 	i = -1;
 	j = -1;
-	start = assign_point(0, 0, 0);
-	end = assign_point(win->map.wide * 50 + 20, win->map.lenght * 50 + 20, 0);
+	start = assign_point(0, 0);
+	end = assign_point(win->map.wide * 50 + 20, win->map.lenght * 50 + 20);
 	cub_drawer(win->img, start, end, 0x909090FF);
-	end = assign_point(50, 50, 0);
+	end = assign_point(50, 50);
 	while (++i < win->map.wide)
 	{
 		j = -1;
 		while (++j < win->map.lenght)
 		{
-			start = assign_point(j * 50 + 10, i * 50 + 10, 0);
+			start = assign_point(j * 50 + 10, i * 50 + 10);
 			if (matrix[i][j] == '1')
 				cub_drawer(win->img, start, end, 0x09005EFF);
-			else if ((matrix[i][j] != ' ' && matrix[i][j] != '\n') ||  !matrix[i][j])
+			else if ((matrix[i][j] != ' ' && matrix[i][j] != '\n')
+				|| !matrix[i][j])
 				cub_drawer(win->img, start, end, 0xB8A649FF);
 		}
 	}
-	player->p = player_drawer(win->img, start, *player, 0xFF322BFF);
+	if(check_inside(win, pos))
+		win->player.p = player_drawer(win, *pos, win->player.angle, 0xFF322BFF);
+	else
+		win->player.p = player_drawer(win, win->player.p, win->player.angle, 0xFF322BFF);
+	t_segm *edge = (t_segm *)ft_calloc(9,sizeof(t_segm));
+	edge[0] = wall(60, 60, 560, 60);
+	edge[1] = wall(60, 60, 60, 360);
+	edge[3] = wall(60, 360, 560, 360);
+	edge[2] = wall(560, 60, 560, 360);
+	edge[4] = wall(50 * 8 + 10, 50 * 5 + 10, 50 * 8 + 10, 50 * 6 + 10);
+	edge[5] = wall(50 * 8 + 10, 50 * 5 + 10, 50 * 9 + 10, 50 * 5 + 10);
+	edge[6] = wall(50 * 8 + 10, 50 * 6 + 10, 50 * 9 + 10, 50 * 6 + 10);
+	edge[7] = wall(50 * 9 + 10, 50 * 5 + 10, 50 * 9 + 10, 50 * 6 + 10);
+	edge[8] = wall(-1, -1, -1, -1);
+	raycast(win, edge, pos, win->player.angle);
 	//printf(" x=%d,y=%d|a=%f ",win->player.p.x, win->player.p.y, win->player.angle);
+	free(edge);
+	edge = NULL;
 }
-
